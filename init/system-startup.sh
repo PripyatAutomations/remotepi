97b26cdf01ace266d7ce4118fbb099aef43b5e5d
Pripyat Automations <84538339+PripyatAutomations@users.noreply.github.com>
2022-11-19 12:10:54 -0500

    cleanup.

--- init/system-startup.sh
+++ init/system-startup.sh
@@ -3,7 +3,13 @@ echo "* Tidying filesytem..."
 sudo mkdir -p /opt/remotepi/logs/asterisk
 sudo chown -R devuan:devuan /opt/remotepi/logs
 sudo chown -R asterisk:devuan /opt/remotepi/logs/asterisk
+
 cd /opt/remotepi/logs/
+
+echo "* Starting pipewire, if needed"
+MYPID=$(pidof pipewire)
+[ "x${MYPID}" != "x" ] && kill -9 ${MYPID}
+pipewire 2>&1 > /opt/remotepi/log/pipewire.log &
 
 echo "* Starting flrig-vnc"
 sudo -u devuan '/opt/remotepi/init/flrig-vnc.start'
